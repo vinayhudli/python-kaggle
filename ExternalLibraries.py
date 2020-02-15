d883c5f09f8cf89e3abdd4469d10187116f36d91
Vinay Deshpande <vinay.deshpande@rakuten.com>
2020-02-15 12:52:06 -0800

    external library import demo

---
+++ ExternalLibraries.py
@@ -0,0 +1,9 @@
+"""
+Working with external libraries
+"""
+
+import sys
+
+print("It's sys and it's type {}".format(type(sys)))
+print("helper functions {}".format(dir(sys)))
+print("accessing path {}".format(type(sys.path)))
