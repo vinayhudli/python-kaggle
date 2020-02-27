92cb3743de618f94c387c9c72de3dfdcfd96b5c0
Vinay Deshpande <vinay.deshpande@rakuten.com>
2020-02-27 07:58:34 -0800

    adding additional leaf to get least mae

--- model_validation.py
+++ model_validation.py
@@ -15,11 +15,11 @@ melbourne_data = pd.read_csv(melbourne_path)
 filtered_data = melbourne_data.dropna(axis=0)
 
 # Choose target
-y = filtered_data.head(4000).Price
+y = filtered_data.Price
 
 # Choose features to predict target
 melbourne_data_features = ['Rooms', 'Bathroom', 'Landsize', 'BuildingArea', 'YearBuilt', 'Lattitude', 'Longtitude']
-x = filtered_data[melbourne_data_features].head(4000)
+x = filtered_data[melbourne_data_features]
 
 #splitting data into training and validation data
 train_x, val_x, train_y, val_y = train_test_split(x, y, random_state=0)
@@ -35,3 +35,16 @@ from sklearn.metrics import mean_absolute_error
 predicted_prices = melbourne_model.predict(X=val_x)
 error = mean_absolute_error(val_y, predicted_prices)
 print("mean error is ",error)
+
+
+# checking the number of leafs that give least MAE
+
+def get_mae(max_leaf_nodes=None):
+    mae_model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    mae_model.fit(train_x, train_y)
+    mae_predicted_prices = mae_model.predict(val_x)
+    return mean_absolute_error(val_y, mae_predicted_prices)
+
+
+for max_leaf in [5, 50, 250, 300, 350, 400, 450, 500, 550, 5000]:
+    print ("for leaf {} mean absolute error {}".format(max_leaf, get_mae(max_leaf)))
